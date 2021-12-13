ac0e1ba574b1455d32095f0dcbe59da34572eede
Komodo07 <komodo.07@outlook.com>
2021-12-12 20:46:19 -0600

    first half of  class update
    - Added Horizontal and Vertical player input
    - Added Colliders and Rigidbodies
    - Created separate script to handle triggers and collissions

--- Delivery Driver/Assets/Scripts/Driver.cs
+++ Delivery Driver/Assets/Scripts/Driver.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField] float steerSpeed = 100.0f;
+    [SerializeField] float moveSpeed = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +16,12 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, 0.1f);
-        transform.Translate(0, 0.01f, 0);
+        float steerAmount = Input.GetAxis("Horizontal") * Time.deltaTime * steerSpeed;
+        float moveAmount = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
     }
+
+
 }
